baa1759188a7ba9b01db3d7a42ca870f59d93b57
Felix Körber <felixandrewkoerber@gmail.com>
2024-02-26 10:26:56 +0100

    _site

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "New paper published: Predicting intelligence from cortical gray matter volume",
+        "excerpt":"Statistically significant prediction (N = 308) but large prediction error questioning the practical value of VBM correlates of general intelligence. A positive association between brain size and intelligence is firmly established, but whether region-specific anatomical differences contribute to general intelligence remains an open question. Results from voxel-based morphometry (VBM) -...","categories": ["blog"],
+        "tags": ["Jekyll","update"],
+        "url": "/blog/welcome-to-jekyll/",
+        "teaser": "/assets/images/FiebachLabLogo.png%22"
+      }]
